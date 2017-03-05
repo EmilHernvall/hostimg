d96efe46ee760967ca69d89c09061bb49bc0b5cd
Emil Hernvall <emil@znaptag.com>
2017-03-05 22:36:10 +0100

    Implemented inotify support

--- src/file.rs
+++ src/file.rs
@@ -7,9 +7,9 @@ use std::fs::{read_dir, File};
 use std::io::Error as IoError;
 use std::io::Result as IoResult;
 use std::io::{Read, BufReader, ErrorKind};
-use std::path::{Path,PathBuf};
+use std::path::{Path,PathBuf,Component};
 use std::sync::mpsc::channel;
-use std::sync::{RwLock, Arc};
+use std::sync::Arc;
 use std::time::Duration;
 use std::collections::BTreeSet;
 
@@ -83,37 +83,37 @@ impl GalleryScanner {
                          accept: &F) -> IoResult<ImageGallery>
         where F: Fn(&Path) -> bool {
 
-        let mut images = BTreeSet::new();
-        let mut galleries = BTreeSet::new();
+        let local_path = dir.strip_prefix(&self.context.gallery_dir)
+            .or(build_io_error("Failed to strip directory prefix"))?
+            .to_path_buf();
+
+        let mut new_gallery = ImageGallery::new(local_path);
+
         for entry in read_dir(dir)?.filter_map(|x| x.ok()) {
             let filetype = entry.file_type()?;
             let p = entry.path();
             if filetype.is_dir() {
                 match self.scan_recursive(&p, accept) {
                     Ok(gallery) => {
-                        galleries.insert(Arc::new(gallery));
+                        if gallery.imagecount > 0 {
+                            new_gallery.imagecount += gallery.imagecount;
+                            new_gallery.sub_galleries.insert(Arc::new(gallery));
+                        }
                     },
                     Err(e) => println!("Failed to add gallery {:?}", e)
                 }
             } else if filetype.is_file() && accept(&p) {
                 match self.add_file(&p) {
                     Ok(info) => {
-                        images.insert(Arc::new(info));
+                        new_gallery.imagecount += 1;
+                        new_gallery.images.insert(Arc::new(info));
                     },
                     Err(e) => println!("Failed to add image {:?}", e)
                 }
             }
         }
 
-        let local_path = dir.strip_prefix(&self.context.gallery_dir)
-            .or(build_io_error("Failed to strip directory prefix"))?
-            .to_path_buf();
-
-        Ok(ImageGallery {
-            path: local_path,
-            sub_galleries: galleries,
-            images: images
-        })
+        Ok(new_gallery)
     }
 
     fn add_file(&mut self, file: &PathBuf) -> IoResult<ImageInfo> {
@@ -162,27 +162,60 @@ impl GalleryScanner {
             .or(build_io_error("Failed to get root gallery"))?;
 
         match event {
-            DebouncedEvent::Create(path) => {
-                if !is_image(&path) {
+            DebouncedEvent::Create(ref path) |
+            DebouncedEvent::Remove(ref path) => {
+                if !is_image(path) {
                     return Ok(());
                 }
 
-                let parent = match path.parent() {
+                let info = Arc::new(self.add_file(path).or(build_io_error("Failed to add file"))?);
+
+                let parent = match path.parent().and_then(|x| x.strip_prefix(&self.context.gallery_dir).ok()) {
                     Some(x) => x.to_path_buf(),
                     None => return build_io_error("Path has no parent")
                 };
 
-                let gallery = root_gallery.find_gallery_from_name(&parent);
+                let op = match event {
+                    DebouncedEvent::Create(_) => {
+                        println!("Found new image: {:?}", path);
+                        GalleryModification::Add(info)
+                    },
+                    DebouncedEvent::Remove(_) => {
+                        println!("Detected removed image: {:?}", path);
+                        GalleryModification::Remove(info)
+                    },
+                    _ => unreachable!()
+                };
 
-                let info = self.add_file(&path).or(build_io_error("Failed to add file"));
+                let new_root = root_gallery.modify(&parent, op)?;
+                self.context.set_root_gallery(new_root)
+                    .or(build_io_error("Failed to set root gallery"))?;
+            },
+            DebouncedEvent::Rename(ref from_path, ref to_path) => {
+                if !is_image(from_path) {
+                    return Ok(());
+                }
 
-                //gallery.
+                println!("Detected rename: {:?} - {:?}", from_path, to_path);
+
+                let from_info = Arc::new(self.add_file(from_path).or(build_io_error("Failed to add file"))?);
+                let from_parent = match from_path.parent().and_then(|x| x.strip_prefix(&self.context.gallery_dir).ok()) {
+                    Some(x) => x.to_path_buf(),
+                    None => return build_io_error("Path has no parent")
+                };
+
+                let to_info = Arc::new(self.add_file(to_path).or(build_io_error("Failed to add file"))?);
+                let to_parent = match to_path.parent().and_then(|x| x.strip_prefix(&self.context.gallery_dir).ok()) {
+                    Some(x) => x.to_path_buf(),
+                    None => return build_io_error("Path has no parent")
+                };
+
+                let new_root = root_gallery.modify(&from_parent, GalleryModification::Remove(from_info))?
+                                           .modify(&to_parent, GalleryModification::Add(to_info))?;
+                self.context.set_root_gallery(new_root)
+                    .or(build_io_error("Failed to set root gallery"))?;
             },
             DebouncedEvent::Write(_) => {
-            },
-            DebouncedEvent::Remove(_) => {
-            },
-            DebouncedEvent::Rename(_, _) => {
             },
             DebouncedEvent::Rescan => {
             },
@@ -220,7 +253,8 @@ fn build_io_error<T>(message: &str) -> IoResult<T> {
 pub struct ImageGallery {
     pub path: PathBuf,
     pub sub_galleries: BTreeSet<Arc<ImageGallery>>,
-    pub images: BTreeSet<Arc<ImageInfo>>
+    pub images: BTreeSet<Arc<ImageInfo>>,
+    pub imagecount: u32
 }
 
 impl Ord for ImageGallery {
@@ -244,7 +278,22 @@ impl PartialEq for ImageGallery {
 impl Eq for ImageGallery {
 }
 
+#[derive(Clone)]
+pub enum GalleryModification {
+    Add(Arc<ImageInfo>),
+    Remove(Arc<ImageInfo>)
+}
+
 impl ImageGallery {
+    pub fn new(path: PathBuf) -> ImageGallery {
+        ImageGallery {
+            path: path,
+            sub_galleries: BTreeSet::new(),
+            images: BTreeSet::new(),
+            imagecount: 0
+        }
+    }
+
     pub fn get_name(&self) -> String {
         self.path.file_name()
             .and_then(|x| x.to_str())
@@ -262,6 +311,76 @@ impl ImageGallery {
         self.path.to_str()
             .map(|x| x.to_string())
             .unwrap_or("".to_string())
+    }
+
+    fn build_next_path_step(&self, dir_path: &PathBuf) -> Option<PathBuf> {
+        let next_component = match dir_path.strip_prefix(&self.path).ok()
+            .and_then(|x| x.components().next()) {
+            Some(x) => x,
+            None => return None
+        };
+
+        match next_component {
+            Component::Normal(next_component) => {
+                let mut new_subpath = self.path.clone();
+                new_subpath.push(next_component);
+
+                Some(new_subpath)
+            },
+            _ => None
+        }
+    }
+
+    pub fn modify(&self, dir_path: &PathBuf, op: GalleryModification) -> IoResult<Arc<ImageGallery>> {
+        let mut new_self = ImageGallery::new(self.path.clone());
+        let mut found_gallery = false;
+        for subgallery in &self.sub_galleries {
+            let new_subgallery = if dir_path.starts_with(&subgallery.path) {
+                found_gallery = true;
+                subgallery.modify(dir_path, op.clone())?
+            } else {
+                subgallery.clone()
+            };
+
+            if new_subgallery.imagecount > 0 {
+                new_self.imagecount += new_subgallery.imagecount;
+                new_self.sub_galleries.insert(new_subgallery);
+            }
+        }
+
+        for image in &self.images {
+            new_self.imagecount += 1;
+            new_self.images.insert(image.clone());
+        }
+
+        if dir_path.as_path() == self.path.as_path() {
+            match op {
+                GalleryModification::Add(info) => {
+                    new_self.images.insert(info.clone());
+                    new_self.imagecount += 1;
+                },
+                GalleryModification::Remove(info) => {
+                    if new_self.images.remove(&info) {
+                        new_self.imagecount -= 1;
+                    }
+                }
+            }
+
+        } else if !found_gallery {
+            match self.build_next_path_step(dir_path) {
+                Some(new_subpath) => {
+                    let new_subgallery = ImageGallery::new(new_subpath)
+                        .modify(dir_path, op.clone())?;
+                    new_self.imagecount += new_subgallery.imagecount;
+                    new_self.sub_galleries.insert(new_subgallery);
+                },
+                None => {
+                    return build_io_error("Failed to find next path step when rebuilding gallery structure");
+                }
+            }
+        }
+
+        Ok(Arc::new(new_self))
     }
 
     pub fn find_gallery_from_name(&self, search_path: &PathBuf) -> Option<Arc<ImageGallery>> {
